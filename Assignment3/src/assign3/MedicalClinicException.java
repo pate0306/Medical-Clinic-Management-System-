2bd93b4c0dce1a7c748313dfe9eaf4127860b748
pate0306 <pate0306@algonquinlive.com>
2019-02-23 22:44:00 -0500

    Java Project

---
+++ Assignment3/src/assign3/MedicalClinicException.java
@@ -0,0 +1,19 @@
+package assign3;
+/* File Name: MedicalClinicException.java
+* Course Name:18F_CST8284_312 Object Oriented Programming
+* Lab Section: 312
+* Student Name: Parth Patel
+* Date: 26 -Nov-2018
+*/
+public class MedicalClinicException extends RuntimeException {
+
+	private String measssage;
+
+	public MedicalClinicException(String message) {
+
+		super(message);
+
+	}
+
+	
+}
